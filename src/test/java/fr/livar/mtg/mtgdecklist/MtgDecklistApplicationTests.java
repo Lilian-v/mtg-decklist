d8848d59f51b13c016717b74a91e3e80699193ff
Lilian VARREL <lilian.varrel@smile.fr>
2023-10-10 17:00:08 +0200

    Init springboot

---
+++ src/test/java/fr/livar/mtg/mtgdecklist/MtgDecklistApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.livar.mtg.mtgdecklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtgDecklistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
